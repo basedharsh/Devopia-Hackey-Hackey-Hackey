3d9b185b42e418c46be7db9375b27e7add9151c5
.androidmanan <manankabra200328@gmail.com>
2024-06-08 11:57:13 +0530

    chat and gemini

--- lib/view/home/fund_info.dart
+++ lib/view/home/fund_info.dart
@@ -1,16 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:trackizer/backend/auth/login.dart';
-import 'package:trackizer/view/home/main_home.dart';
-import 'package:trackizer/view/login/sign_up_view.dart';
-import 'package:trackizer/view/main_tab/main_tab_view.dart';
-
-import '../../common/color_extension.dart';
-import '../../common_widget/primary_button.dart';
-import '../../common_widget/round_textfield.dart';
-import '../../common_widget/secondary_boutton.dart';
+import 'package:http/http.dart' as http;
+import 'package:trackizer/common/color_extension.dart';
+import 'package:trackizer/common_widget/primary_button.dart';
+import 'package:trackizer/common_widget/round_textfield.dart';
 
 class FundInfoPage extends StatefulWidget {
   const FundInfoPage({Key? key}) : super(key: key);
@@ -20,11 +13,11 @@ class FundInfoPage extends StatefulWidget {
 }
 
 class _FundInfoPageState extends State<FundInfoPage> {
-  TextEditingController mutualFund = TextEditingController();
-  TextEditingController fixedDeposit = TextEditingController();
-  TextEditingController crypto = TextEditingController();
-  TextEditingController stocks = TextEditingController();
-  TextEditingController bonds = TextEditingController();
+  TextEditingController budget = TextEditingController();
+  TextEditingController maxLossEndured = TextEditingController();
+  TextEditingController age = TextEditingController();
+  TextEditingController bucket = TextEditingController();
+  TextEditingController ratioLow = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -37,101 +30,62 @@ class _FundInfoPageState extends State<FundInfoPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               RoundTextField(
-                title: "Mutual Fund",
-                controller: mutualFund,
+                title: "Budget",
+                controller: budget,
                 keyboardType: TextInputType.number,
               ),
               const SizedBox(height: 15),
               RoundTextField(
-                title: "Fixed Deposit",
-                controller: fixedDeposit,
+                title: "Max Loss Endured",
+                controller: maxLossEndured,
                 keyboardType: TextInputType.number,
               ),
               const SizedBox(height: 15),
               RoundTextField(
-                title: "Crypto",
-                controller: crypto,
+                title: "Age",
+                controller: age,
                 keyboardType: TextInputType.number,
               ),
               const SizedBox(height: 15),
               RoundTextField(
-                title: "Stocks",
-                controller: stocks,
-                keyboardType: TextInputType.number,
+                title: "Bucket",
+                controller: bucket,
               ),
               const SizedBox(height: 15),
               RoundTextField(
-                title: "Bonds",
-                controller: bonds,
+                title: "Ratio Low",
+                controller: ratioLow,
                 keyboardType: TextInputType.number,
               ),
               const SizedBox(height: 15),
               PrimaryButton(
                 title: "Get Started",
-                onPressed: () {
-                  double mutualFundAmount = double.parse(mutualFund.text);
-                  double fixedDepositAmount = double.parse(fixedDeposit.text);
-                  double cryptoAmount = double.parse(crypto.text);
-                  double stocksAmount = double.parse(stocks.text);
-                  double bondsAmount = double.parse(bonds.text);
+                onPressed: () async {
+                  // String url = "http://127.0.0.1:5000/create_portfolio";
+                  String url = "http://10.0.2.2:5000/create_portfolio";
+                  var data = [
+                    {
+                      'emp': 1,
+                      'budget': int.parse(budget.text),
+                      'max_loss_endured': int.parse(maxLossEndured.text),
+                      'age': int.parse(age.text),
+                      'bucket': bucket.text,
+                      'ratio_low': double.parse(ratioLow.text)
+                    }
+                  ];
 
-                  double totalInvestment = mutualFundAmount +
-                      fixedDepositAmount +
-                      cryptoAmount +
-                      stocksAmount +
-                      bondsAmount;
+                  final response = await http.post(
+                    Uri.parse(url),
+                    headers: {"Content-Type": "application/json"},
+                    body: jsonEncode({"employees": data}),
+                  );
 
-                  double mutualFundPercentage =
-                      (mutualFundAmount / totalInvestment) * 100;
-                  double fixedDepositPercentage =
-                      (fixedDepositAmount / totalInvestment) * 100;
-                  double cryptoPercentage =
-                      (cryptoAmount / totalInvestment) * 100;
-                  double stocksPercentage =
-                      (stocksAmount / totalInvestment) * 100;
-                  double bondsPercentage =
-                      (bondsAmount / totalInvestment) * 100;
-
-                  String finalRisk;
-                  bool highRisk = (stocksPercentage + cryptoPercentage) > 33;
-                  bool lowRisk =
-                      (bondsPercentage + fixedDepositPercentage) > 33;
-                  bool mediumRisk = mutualFundPercentage > 33;
-
-                  if (highRisk) {
-                    finalRisk = "High";
-                  } else if (mediumRisk) {
-                    finalRisk = "Medium";
-                  } else if (lowRisk) {
-                    finalRisk = "Low";
+                  if (response.statusCode == 200) {
+                    var jsonResponse = jsonDecode(response.body);
+                    print(jsonResponse);
                   } else {
-                    finalRisk =
-                        "Undefined"; // In case none of the conditions are met
+                    print('Failed to get data from server');
                   }
-
-                  FirebaseFirestore.instance
-                      .collection("users")
-                      .doc(FirebaseAuth.instance.currentUser!.uid)
-                      .update({
-                    "MutualFund": mutualFund.text,
-                    "FixedDeposit": fixedDeposit.text,
-                    "Crypto": crypto.text,
-                    "Stocks": stocks.text,
-                    "Bonds": bonds.text,
-                    "totalInvestment": totalInvestment.toString(),
-                    "mutualFundPercentage": mutualFundPercentage.toString(),
-                    "fixedDepositPercentage": fixedDepositPercentage.toString(),
-                    "cryptoPercentage": cryptoPercentage.toString(),
-                    "stocksPercentage": stocksPercentage.toString(),
-                    "bondsPercentage": bondsPercentage.toString(),
-                    "Current Risk": finalRisk,
-                  }).then((value) {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => MainHome(),
-                      ),
-                    );
-                  });
                 },
               ),
               const Spacer(),
